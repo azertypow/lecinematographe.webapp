fc39e527f1979f1f6ae278a2c8ffcd9afde5440c
azertypow <azertypow@gmail.com>
2024-03-17 08:52:38 +0100

    feat: map component for content page

---
+++ components/content/AppContentMap.vue
@@ -0,0 +1,28 @@
+<template>
+    <div
+        class="v-app-content-map"
+    >
+        <AppMap/>
+    </div>
+</template>
+
+
+
+
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps<{
+    message?: string
+}>()
+</script>
+
+
+
+
+
+<style lang="scss" scoped >
+.v-app-content-map {
+}
+</style>
