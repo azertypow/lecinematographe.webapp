a9f93bc64157f51fd716a3853db2ae24680f2ab0
azertypow <nico@villa1203.ch>
2024-07-11 13:59:01 +0200

    feat: film timeline 90 days

--- _utils/apiTicket.ts
+++ _utils/apiTicket.ts
@@ -18,7 +18,7 @@ export async function fetchFromTicketAPI<T>(endPoint: string): Promise<T> {
 }
 
 export async function apiGetFilmList() {
-    return fetchFromTicketAPI<IFilmListResponse>('/ajax/WwtLstFilm.php?WwtTrie=4&WwtLimi=60')
+    return fetchFromTicketAPI<IFilmListResponse>('/ajax/WwtLstFilm.php?WwtTrie=4&WwtLimi=90')
 
 }
 
