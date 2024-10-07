f5491e7e42d38c3be667e1e6f3c89e899bf37c18
azertypow <nico@villa1203.ch>
2024-10-07 19:53:44 +0200

    feat: create tecktack fetching function to get screening by ID

--- _utils/ticketackFetch.ts
+++ _utils/ticketackFetch.ts
@@ -1,4 +1,5 @@
 import {LECINEMATOGRAPHE_ENGINE_TOKEN} from "~/_utils/LECINEMATOGRAPHE_ENGINE_TOKEN";
+import type {ApiTicketack_screening} from "~/_utils/apiTicketack";
 
 const ticketackApiBaseUrl = 'https://lecinematographe-engine.ticketack.com/api'
 
@@ -28,5 +29,26 @@ export async function ticketackApi_screenings(queries: {
     } catch (error) {
         console.error(error)
         return []
+    }
+}
+
+export async function ticketackApi_screening_byID(UUID: string) {
+
+    const url = `${ticketackApiBaseUrl}/screenings/${UUID}`;
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'x-api-key': LECINEMATOGRAPHE_ENGINE_TOKEN,
+        },
+    };
+
+    try {
+        const response = await fetch(url, options);
+        return await response.json() as ApiTicketack_screening
+    } catch (error) {
+        console.error(error)
+        return null
     }
 }
