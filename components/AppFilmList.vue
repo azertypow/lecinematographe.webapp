9a3125a711ca6562b2d9f00b32532f491fdba784
azertypow <nico@villa1203.ch>
2025-01-17 16:06:26 +0100

    feat: right spacing scroll width

--- components/AppFilmList.vue
+++ components/AppFilmList.vue
@@ -52,8 +52,8 @@
       </div>
       <div  class="app-flex__shrink-0 v-app-film-list__films-container__item"
             style="
-              width: 25vw!important;
-              flex-basis: 25vw!important;
+              width:      5vw!important;
+              flex-basis: 5vw!important;
             "
       ></div>
     </div>
